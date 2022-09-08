9c33255eb67e967b20e93933b54377c7e0cf68d6
BaeCheolHan <joker10421@gmail.com>
2022-09-08 11:29:29 +0900

    test

--- src/main/java/com/herren/gongassistance/shop/controller/ShopController.java
+++ src/main/java/com/herren/gongassistance/shop/controller/ShopController.java
@@ -41,6 +41,8 @@ public class ShopController {
     @DeleteMapping
     public BaseResponse deleteShop(@PathVariable(name = "shopId") Long shopId) {
         shopService.deleteShop(shopId);
+
+
         return BaseResponse.builder().code(GongAssistanceCode.SUCCESS).message(GongAssistanceCode.SUCCESS.getMessage()).build();
     }
 }
